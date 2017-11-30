e05f1e7fb03be427f2e6edd9f1f7d98be35def2d
xjflyttp <xjflyttp@gmail.com>
2017-11-30 11:15:02 +0800

    fix basePath

--- TimesheetAsset.php
+++ TimesheetAsset.php
@@ -12,7 +12,6 @@ use yii\web\AssetBundle;
 class TimesheetAsset extends AssetBundle {
 
     public $sourcePath = '@bower/timesheet-for-yii2/dist';
-    public $basePath = '@webroot/assets';
     public $css = ['timesheet.css'];
     public $js = ['timesheet.js'];
 
